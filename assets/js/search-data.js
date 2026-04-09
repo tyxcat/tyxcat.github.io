803dd3787e68ddd7476bcbda61cea263616fb75f
tyxcat <txu2025@gmail.com>
2026-04-09 01:52:08 +0000

    Deploying to gh-pages from @ tyxcat/tyxcat.github.io@f03cb5ac42aecb9436f9ddde44052bb55c6e47dc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -42,14 +42,30 @@ ninja.data = [{
           section: "News",},{id: "news-project-moxart-has-been-accepted-to-chi-2026",
           title: 'Project MoXaRt has been accepted to CHI 2026.',
           description: "",
+          section: "News",},{id: "news-our-paper-gazeanywhere-has-been-accepted-to-cvpr-2026-the-first-foundation-model-for-promptable-gaze-target-estimation",
+          title: 'Our paper GazeAnywhere has been accepted to CVPR 2026! 🎉 The first foundation...',
+          description: "",
           section: "News",},{id: "news-thrilled-to-be-a-speaker-at-the-2026-silicon-valley-women-in-engineering-conference-i-ll-be-presenting-sound-space-and-agency-building-context-aware-wearable-systems-in-the-emerging-technologies-c2-ux-amp-amp-wearable-technology-session-sat-3-21-at-1-45-2-45-pm-️",
           title: 'Thrilled to be a speaker at the 2026 Silicon Valley Women in Engineering...',
           description: "",
-          section: "News",},{id: "projects-moxart-audio-visual-object-guided-sound-interaction-for-xr",
+          section: "News",},{id: "news-presenting-moxart-at-chi-2026-in-barcelona-come-find-us-at-the-barcelona-international-convention-centre-p1-room-128-on-fri-apr-17-at-9-00-am",
+          title: 'Presenting MoXaRt at CHI 2026 in Barcelona! 🇪🇸 Come find us at the...',
+          description: "",
+          section: "News",},{id: "projects-gazeanywhere-gaze-target-estimation-anywhere-with-concepts",
+          title: 'GazeAnywhere: Gaze Target Estimation Anywhere with Concepts',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/gazeanywhere/";
+            },},{id: "projects-moxart-audio-visual-object-guided-sound-interaction-for-xr",
           title: 'MoXaRt: Audio-Visual Object-Guided Sound Interaction for XR',
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/moxart/";
+            },},{id: "projects-samosa-enhancing-xr-auditory-realism-via-multimodal-scene-aware-acoustic-rendering",
+          title: 'SAMOSA: Enhancing XR Auditory Realism via Multimodal Scene-Aware Acoustic Rendering',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/samosa/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
